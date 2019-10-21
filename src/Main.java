404bd88f8e98b04f8e2f596e3287a2aac504e272
petivip <petivip@gmail.com>
2019-10-21 16:18:05 -0400

    new change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String [] args){
+        //Printing out hello world to the console
         System.out.println("Hello world");
-    }
+        //declaration of a variable
+        String name;
+       //initialization of a variable
+        name="Petros";
+        //declaration and initialization of a variable in the same line
+        String newName="Brand";
+        //reinitialzing a variable
+        name="Danile";
+        System.out.println(name);
+              }
 }
